bbe3ad388c4a2410586f66401beed7c2bd0d992f
Hidetoshi Yajima <hyajima.1014@gmail.com>
2019-02-13 22:51:35 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strconv"
@@ -8,7 +9,9 @@ import (
 	"time"
 
 	"github.com/ashwanthkumar/slack-go-webhook"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -17,16 +20,7 @@ import (
 )
 
 const (
-	SSMSEARCHWORD = "_base_ami"
-	SSMPAGE       = 3
-	LCPAGE        = 4
-	AMIDELETEDATE = -14
-	LCDELETEDATE  = -14
-
-	// Slack
-	WEBHOOKURL = "https://hooks.slack.com/services/XXXXX"
-	CHANNEL    = "dev"
-	USERNAME   = "AutoDeleteAMIs"
+	SSMSEARCHWORD = "_source_ami"
 )
 
 var (
@@ -36,16 +30,24 @@ var (
 	svcASG = autoscaling.New(session.New(&config))
 	svcIAM = iam.New(session.New(&config))
 	layout = "2006-01-02T15:04:05"
+
+	SLACKURL      = os.Getenv("SLACKURL")
+	CHANNEL       = os.Getenv("CHANNEL")
+	USERNAME      = os.Getenv("USERNAME")
+	AMIEXPIREDATE = os.Getenv("AMIEXPIREDATE")
+	LCEXPIREDATE  = os.Getenv("LCEXPIREDATE")
+	SSMPAGE       = os.Getenv("SSMPAGE")
+	LCPAGE        = os.Getenv("LCPAGE")
 )
 
-/* SSMパラメータ Interface */
+// SSM Interface
 type SSM interface {
 	FetchSSMParamsKey()
 	FetchSSMParamsValue(*string) string
 	GetAMIFromSSM()
 }
 
-/* ASG Interface */
+// ASG Interface
 type ASG interface {
 	FetchListLCNameCreatedTime()
 	FetchLCName()
@@ -55,29 +57,29 @@ type ASG interface {
 	DeleteLC()
 }
 
-/* AMI Interface */
+// AMI Interface
 type AMI interface {
 	FetchListAMI()
 	FetchSnapshotIDFromAMI([][]string)
 	FetchThresholdAMI()
-	DeleteSnapshotByID()
+	DeleteSnapshotBySnapshotID()
 	DeregisterAMIByAMIID(string)
 }
 
-/* Slack Interface */
+// Slack Interface
 type Slack interface {
 	PostSlack()
 	GetAccountAlias()
 }
 
-/* SSMParamsの構造体 */
+// SSMParams struct
 type SSMParams struct {
 	key      []string
 	value    string
-	amiIdSSM []string
+	amiIDSSM []string
 }
 
-/* ASGParamsの構造体 */
+// ASGParams struct
 type ASGParams struct {
 	lcNameFromASG     []string
 	lcNameList        []string
@@ -88,17 +90,19 @@ type ASGParams struct {
 	deleteFlag        bool
 }
 
-/* AMIParamsの構造体 */
+// AMIParams struct
 type AMIParams struct {
-	amiList      [][]string
-	snapshotID   string
-	threshold    string
-	excludedList []string
-	deleteFlag   bool
-	deleteCount  int
+	amiList             [][]string
+	snapshotID          string
+	existSnapshots      int
+	threshold           string
+	excludedList        []string
+	deleteFlag          bool
+	deleteAMICount      int
+	deleteSnapshotCount int
 }
 
-/* SlackParamsの構造体 */
+// SlackParams struct
 type SlackParams struct {
 	accountAlias string
 	webhookurl   string
@@ -107,11 +111,16 @@ type SlackParams struct {
 }
 
 func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context) {
+
 	// SSM
 	sp := &SSMParams{}
 	sp.FetchSSMParamsKey()
 	sp.GetAMIFromSSM()
-	fmt.Printf("SSM AMI List: \n%s\n\n", sp.amiIdSSM)
+	fmt.Printf("SSM AMI List: \n%s\n\n", sp.amiIDSSM)
 
 	// LauchConfig(LC)
 	ap := &ASGParams{}
@@ -121,8 +130,9 @@ func main() {
 
 	// AMI
 	amip := &AMIParams{}
-	amip.excludedList = append(sp.amiIdSSM, ap.amiIDLCFromASG...)
-	fmt.Printf("Exculuded AMI List: \n%v\n\n", amip.excludedList)
+	amip.excludedList = append(sp.amiIDSSM, ap.amiIDLCFromASG...)
+	fmt.Printf("Exculuded AMI List: \n%s\n\n", amip.excludedList)
+	amip.FetchSnapshotList()
 	amip.FetchListAMI()
 	amip.FetchThresholdAMI()
 	amip.DeleteAMISnapshot()
@@ -130,28 +140,53 @@ func main() {
 	// LaunchConfig Delete
 	ap.FetchListLCNameCreatedTime()
 	ap.FetchThresholdLC()
+	fmt.Printf("Exculuded LaunchConfig List: \n%s\n\n", ap.lcNameFromASG)
 	ap.DeleteLC()
 
 	// Slack
 	s := &SlackParams{}
 	s.GetAccountAlias()
-
-	excludedListSSM := strings.Join(sp.amiIdSSM, " / ")
-	excludedListLC := strings.Join(ap.amiIDLCFromASG, " / ")
-	s.PostSlack(s.accountAlias, len(amip.amiList), amip.deleteCount, ap.deleteCount, excludedListSSM, excludedListLC)
+	excludedListAMISSM := strings.Join(sp.amiIDSSM, " / ")
+	excludedListAMILC := strings.Join(ap.amiIDLCFromASG, " / ")
+	excludedListLC := strings.Join(ap.lcNameFromASG, " / ")
+	s.PostSlack(s.accountAlias, len(amip.amiList), amip.deleteAMICount, amip.existSnapshots, amip.deleteSnapshotCount, len(ap.lcNameList), ap.deleteCount, excludedListAMISSM, excludedListAMILC, excludedListLC)
 }
 
-/* AMIの削除期間の閾値を設定 */
+// Snapshotのリストを取得
+func (amip *AMIParams) FetchSnapshotList() {
+	var owner, snapshotId []*string
+	var _owner = []string{"self"}
+	owner = aws.StringSlice(_owner)
+	params := &ec2.DescribeSnapshotsInput{
+		SnapshotIds: snapshotId,
+		OwnerIds:    owner,
+	}
+	res, err := svcEC2.DescribeSnapshots(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	amip.existSnapshots = len(res.Snapshots)
+	//fmt.Println(res)
+}
+
+// AMI削除の閾値を取得
 func (amip *AMIParams) FetchThresholdAMI() {
+
+	// convert string to int
+	var _AMIEXPIREDATE int
+	_AMIEXPIREDATE, _ = strconv.Atoi(AMIEXPIREDATE)
+
 	t := time.Now()
-	thresholdTime := t.AddDate(0, 0, AMIDELETEDATE)
+	thresholdTime := t.AddDate(0, 0, _AMIEXPIREDATE)
 	amip.threshold = thresholdTime.Format(layout)
 }
 
-/* AMIのリストを取得 */
+// AMIのリストを取得
 func (amip *AMIParams) FetchListAMI() {
 	var owner, images []*string
-	var _owner []string = []string{"self"}
+	var _owner = []string{"self"}
 
 	owner = aws.StringSlice(_owner)
 	params := &ec2.DescribeImagesInput{
@@ -164,6 +199,7 @@ func (amip *AMIParams) FetchListAMI() {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+	//fmt.Println(res)
 	for _, resImage := range res.Images {
 
 		amiInfo := []string{
@@ -174,11 +210,11 @@ func (amip *AMIParams) FetchListAMI() {
 	}
 }
 
-/* AMIからSnapshot-IDを取得 */
-func (amip *AMIParams) FetchSnapshotIDFromAMI(amiId string) {
+// AMI-IDでSnapshot-IDを取得
+func (amip *AMIParams) FetchSnapshotIDFromAMI(amiID string) {
 	params := &ec2.DescribeImagesInput{
 		ImageIds: []*string{
-			aws.String(amiId),
+			aws.String(amiID),
 		},
 	}
 	res, err := svcEC2.DescribeImages(params)
@@ -197,73 +233,87 @@ func (amip *AMIParams) FetchSnapshotIDFromAMI(amiId string) {
 	}
 }
 
-/* AMIとSnapshotの削除リクエスト */
+// AMIとSnapshotの削除リクエスト
 func (amip *AMIParams) DeleteAMISnapshot() {
 
-	amip.deleteCount = 0
 	fmt.Println("AMI\t\t\tCreateDated\t\t\tSnapshot")
 	fmt.Println("---------------------------------")
-	for i, _ := range amip.amiList {
+	for i := range amip.amiList {
 
 		if amip.amiList[i][1] < amip.threshold {
-			for j, _ := range amip.excludedList {
+			for j := range amip.excludedList {
 				if amip.amiList[i][0] == amip.excludedList[j] {
 					//fmt.Println(amip.list[i][1])
 					amip.deleteFlag = false
 				}
 			}
 			// 不要なAMIを削除 deleteCheckFlag is true -> delete
+			// Deregister AMIs and Delete Snapshots
 			if amip.deleteFlag == true {
-				//amip.DeregisterAMIByAMIID(amip.amiList[i][0])
+				// 実行順注意(FetchしてAMI削除、Snapshot削除の順番)
 				amip.FetchSnapshotIDFromAMI(amip.amiList[i][0])
-				//amip.DeleteSnapshotByID()
+				amip.DeregisterAMIByAMIID(amip.amiList[i][0])
+				amip.DeleteSnapshotBySnapshotID(amip.snapshotID)
 				fmt.Printf("%s\t%s\t%s\n", amip.amiList[i][0], amip.amiList[i][1], amip.snapshotID)
-				amip.deleteCount++
 			}
 			amip.deleteFlag = true
 		}
 	}
 	fmt.Println("---------------------------------\n")
-	fmt.Printf("Number of Deleted AMIs and Snapshots: %d\n\n", amip.deleteCount)
+	fmt.Printf("Number of Deleted AMIs and Snapshots: %d\n\n", amip.deleteAMICount)
 }
 
-/* AMI-IDを指定して登録解除処理 */
-func (amip *AMIParams) DeregisterAMIByAMIID(deleteId string) {
+// AMI-IDを指定してAMIの登録解除
+func (amip *AMIParams) DeregisterAMIByAMIID(deleteID string) {
 
-	var _deleteId *string
-	_deleteId = &deleteId
+	var _deleteID *string
+	_deleteID = &deleteID
 
 	params := &ec2.DeregisterImageInput{
-		ImageId: _deleteId,
+		ImageId: _deleteID,
 	}
 	_, err := svcEC2.DeregisterImage(params)
-
+	amip.deleteAMICount++
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 }
 
-/* Snapshot-IDを指定して削除処理 */
-func (amip *AMIParams) DeleteSnapshotByID() {
+// Snapshot-IDを指定して削除処理
+func (amip *AMIParams) DeleteSnapshotBySnapshotID(_snapshotID string) {
 	params := &ec2.DeleteSnapshotInput{
-		SnapshotId: aws.String(amip.snapshotID),
+		SnapshotId: aws.String(_snapshotID),
 	}
 	_, err := svcEC2.DeleteSnapshot(params)
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
 	}
+	amip.deleteSnapshotCount++
+	//fmt.Println(_snapshotID)
 }
 
-/* LaunchConfigの削除期間の閾値を設定 */
+// LaunchConfigの削除期間の閾値を設定
 func (ap *ASGParams) FetchThresholdLC() {
+
+	// convert string to int
+	var _LCEXPIREDATE int
+	_LCEXPIREDATE, _ = strconv.Atoi(LCEXPIREDATE)
+
 	t := time.Now()
-	thresholdTime := t.AddDate(0, 0, LCDELETEDATE)
+	thresholdTime := t.AddDate(0, 0, _LCEXPIREDATE)
 	ap.threshold = thresholdTime.Format(layout)
 }
 
-/* LaunchConfig名を指定して削除処理 */
+// LaunchConfig名を指定して削除処理
 func (ap *ASGParams) DeleteLCByLCName(lcName string) {
 	params := &autoscaling.DeleteLaunchConfigurationInput{
 		LaunchConfigurationName: aws.String(lcName),
@@ -273,40 +323,44 @@ func (ap *ASGParams) DeleteLCByLCName(lcName string) {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+	ap.deleteCount++
 }
 
-/* LaunchConfigの削除リクエスト */
+// LaunchConfigの削除リクエスト
 func (ap *ASGParams) DeleteLC() {
 	fmt.Println("LaunchConfig\t\t\t\tCreateDate")
 	fmt.Println("---------------------------------")
 
-	for i, _ := range ap.lcNameList {
+	for i := range ap.lcNameList {
 		if ap.lcCreatedTimeList[i] < ap.threshold {
-			for j, _ := range ap.lcNameFromASG {
+			for j := range ap.lcNameFromASG {
 				if ap.lcNameList[i] == ap.lcNameFromASG[j] {
 					ap.deleteFlag = false
 				}
 			}
 			if ap.deleteFlag == true {
 				fmt.Printf("%s\t%s\n", ap.lcNameList[i], ap.lcCreatedTimeList[i])
-				// ap.DeleteLCByLCName(ap.lcNameList[i])
-				ap.deleteCount++
+				ap.DeleteLCByLCName(ap.lcNameList[i])
 			}
 			ap.deleteFlag = true
 		}
 	}
 	fmt.Println("---------------------------------\n")
-	fmt.Printf("Number of Delete LCs: %d\n", ap.deleteCount)
+	fmt.Printf("Number of Deleted LCs: %d\n", ap.deleteCount)
 }
 
-/* LaunchConfigのAMI-IDを取得 */
+// LaunchConfigのAMI-IDを取得
 func (ap *ASGParams) FetchAMIFromLC() {
+
+	// convert string to int
+	var _LCPAGE int
+	_LCPAGE, _ = strconv.Atoi(LCPAGE)
 	pageNum := 0
 
 	params := &autoscaling.DescribeLaunchConfigurationsInput{}
 	err := svcASG.DescribeLaunchConfigurationsPages(params,
 		func(page *autoscaling.DescribeLaunchConfigurationsOutput, lastPage bool) bool {
-			for i, _ := range ap.lcNameFromASG {
+			for i := range ap.lcNameFromASG {
 				for _, value := range page.LaunchConfigurations {
 					if ap.lcNameFromASG[i] == *value.LaunchConfigurationName {
 						ap.amiIDLCFromASG = append(ap.amiIDLCFromASG, *value.ImageId)
@@ -314,7 +368,7 @@ func (ap *ASGParams) FetchAMIFromLC() {
 				}
 			}
 			pageNum++
-			return pageNum <= LCPAGE
+			return pageNum <= _LCPAGE
 		})
 	if err != nil {
 		fmt.Println(err.Error())
@@ -322,8 +376,12 @@ func (ap *ASGParams) FetchAMIFromLC() {
 	}
 }
 
-/* LaunchConfigのリストと作成日時を取得 */
+// LaunchConfigのリストと作成日時を取得
 func (ap *ASGParams) FetchListLCNameCreatedTime() {
+
+	// convert string to int
+	var _LCPAGE int
+	_LCPAGE, _ = strconv.Atoi(LCPAGE)
 
 	var lcTypeTime time.Time
 	var lcTypeStr string
@@ -341,7 +399,7 @@ func (ap *ASGParams) FetchListLCNameCreatedTime() {
 				ap.lcCreatedTimeList = append(ap.lcCreatedTimeList, lcTypeStr)
 			}
 			pageNum++
-			return pageNum <= LCPAGE
+			return pageNum <= _LCPAGE
 		})
 
 	if err != nil {
@@ -350,7 +408,7 @@ func (ap *ASGParams) FetchListLCNameCreatedTime() {
 	}
 }
 
-/* LaunchConfig名を取得 */
+// LaunchConfig名を取得
 func (ap *ASGParams) FetchLCName() {
 
 	params := &autoscaling.DescribeAutoScalingGroupsInput{}
@@ -365,16 +423,21 @@ func (ap *ASGParams) FetchLCName() {
 	}
 }
 
-/* SSMパラメータからSourceAMI-IDを取得 */
+// SSMパラメータからSourceAMI-IDを取得
 func (sp *SSMParams) GetAMIFromSSM() {
-	for i, _ := range sp.key {
-		sp.amiIdSSM = append(sp.amiIdSSM, sp.FetchSSMParamsValue(&sp.key[i]))
+	for i := range sp.key {
+		sp.amiIDSSM = append(sp.amiIDSSM, sp.FetchSSMParamsValue(&sp.key[i]))
 	}
 }
 
-/* SSMパラメータから `_source_ami` に該当するkeyを取得 */
+// SSMパラメータから `_source_ami` に該当するkeyを取得
 func (sp *SSMParams) FetchSSMParamsKey() {
 	pageNum := 0
+
+	// convert string to int
+	var _SSMPAGE int
+	_SSMPAGE, _ = strconv.Atoi(SSMPAGE)
+
 	params := &ssm.DescribeParametersInput{}
 	err := svcSSM.DescribeParametersPages(params,
 		func(page *ssm.DescribeParametersOutput, lastPage bool) bool {
@@ -384,7 +447,7 @@ func (sp *SSMParams) FetchSSMParamsKey() {
 				}
 			}
 			pageNum++
-			return pageNum <= SSMPAGE
+			return pageNum <= _SSMPAGE
 		})
 	if err != nil {
 		fmt.Println(err.Error())
@@ -392,7 +455,7 @@ func (sp *SSMParams) FetchSSMParamsKey() {
 	}
 }
 
-/* SSMパラメータで指定したKeyのValueを返す */
+// SSMパラメータで指定したKeyのValueを返す
 func (sp *SSMParams) FetchSSMParamsValue(sourceAMI *string) string {
 	params := &ssm.GetParameterInput{
 		Name: sourceAMI,
@@ -407,7 +470,7 @@ func (sp *SSMParams) FetchSSMParamsValue(sourceAMI *string) string {
 	return sp.value
 }
 
-/* AccountAliasを取得 */
+// AccountAliasを取得
 func (s *SlackParams) GetAccountAlias() {
 	params := &iam.ListAccountAliasesInput{}
 	res, err := svcIAM.ListAccountAliases(params)
@@ -416,24 +479,28 @@ func (s *SlackParams) GetAccountAlias() {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	if res.AccountAliases == nil{
+	if res.AccountAliases == nil {
 		s.accountAlias = "None"
-	}else{
+	} else {
 		s.accountAlias = *res.AccountAliases[0]
 	}
 }
 
-/* 結果をSlackに投稿 */
-func (s *SlackParams) PostSlack(accountName string, beforeExistAMIs int, deleteAMIs int, deleteLCs int, excludedListSSM string, excludedListLC string) {
+// 結果をSlackに投稿
+func (s *SlackParams) PostSlack(accountName string, existAMIs int, deleteAMIs int, existSnapshots int, deleteSnapshots int, existLCs int, deleteLCs int, excludedListAMISSM string, excludedListAMILC string, excludedListLC string) {
 	field1 := slack.Field{Title: "Account", Value: accountName}
-	field2 := slack.Field{Title: "Before Total Exist AMIs", Value: strconv.Itoa(beforeExistAMIs)}
-	field3 := slack.Field{Title: "Total Delete AMIs and Snapshots", Value: strconv.Itoa(deleteLCs)}
-	field4 := slack.Field{Title: "Total Delete LauchConfigs", Value: strconv.Itoa(deleteAMIs)}
-	field5 := slack.Field{Title: "Excluded AMIs ( SSM )", Value: excludedListSSM}
-	field6 := slack.Field{Title: "Excluded AMIs ( LC )", Value: excludedListLC}
+	field2 := slack.Field{Title: "Exist AMIs", Value: strconv.Itoa(existAMIs)}
+	field3 := slack.Field{Title: "Deleted AMIs", Value: strconv.Itoa(deleteAMIs)}
+	field4 := slack.Field{Title: "Exist Snapshots", Value: strconv.Itoa(existSnapshots)}
+	field5 := slack.Field{Title: "Deleted Snapshots", Value: strconv.Itoa(deleteSnapshots)}
+	field6 := slack.Field{Title: "Exist LCs", Value: strconv.Itoa(existLCs)}
+	field7 := slack.Field{Title: "Deleted LCs", Value: strconv.Itoa(deleteLCs)}
+	field8 := slack.Field{Title: "Excluded AMIs List ( SSM )", Value: excludedListAMISSM}
+	field9 := slack.Field{Title: "Excluded AMIs List ( LC )", Value: excludedListAMILC}
+	field10 := slack.Field{Title: "Excluded LCs List", Value: excludedListLC}
 
 	attachment := slack.Attachment{}
-	attachment.AddField(field1).AddField(field2).AddField(field3).AddField(field4).AddField(field5).AddField(field6)
+	attachment.AddField(field1).AddField(field2).AddField(field3).AddField(field4).AddField(field5).AddField(field6).AddField(field7).AddField(field8).AddField(field9).AddField(field10)
 	color := "warning"
 	attachment.Color = &color
 	payload := slack.Payload{
@@ -441,7 +508,7 @@ func (s *SlackParams) PostSlack(accountName string, beforeExistAMIs int, deleteA
 		Channel:     s.channel,
 		Attachments: []slack.Attachment{attachment},
 	}
-	err := slack.Send(WEBHOOKURL, "", payload)
+	err := slack.Send(SLACKURL, "", payload)
 	if len(err) > 0 {
 		os.Exit(1)
 	}
